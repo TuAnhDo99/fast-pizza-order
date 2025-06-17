69ac3620e618d75f93b1d32bd164e3491341195b
TuAnhDo99 <lobhestia@gmail.com>
2025-06-17 09:16:24 +0700

    Fix login bug and update styles

--- src/store.js
+++ src/store.js
@@ -1,12 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import userReducer from './features/user/userSlice'
 import cartReducer from './features/cart/cartSlice'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // localStorage for web
 
-const store = configureStore({
-    reducer: {
-        user: userReducer,
-        cart: cartReducer,
-    },
+const rootReducer = combineReducers({
+    cart: cartReducer,
+    user: userReducer,
+    // Add other reducers here, e.g. menu, order if you have them
 })
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart', 'user'], // Optional: specify slices to persist
+    // blacklist: []        // Optional: slices to exclude
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+    reducer: persistedReducer,
+})
+
+export const persistor = persistStore(store)
 export default store
